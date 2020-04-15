7c62aaaa96e81a41fb12cf74ddbbaf2d65bf2797
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-15 10:39:09 -0700

    add test with meaningful fail for character object

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -2,9 +2,9 @@ import { Character } from "./../src/rpg.js";
 
 describe ('Character', () => {
   test('should create a character with a name, empty inventory, character class, experience points of 0, level 1, and first power', () => {
-    let character = new Character (userName, userClass, userPower);
-    expect(character.name).toEqual(userName);
-    expect(character.class).toEqual(userClass);
-    expect(character.power).toEqual([userPower]);
+    let character = new Character ("Jessica", "Wizard", "Magic Missle");
+    expect(character.name).toEqual("Jessica");
+    expect(character.class).toEqual("Wizard");
+    expect(character.power).toEqual(["Magic Missle"]);
   });
 });
