9c40018e625adca1474483d4ff394acb0ca063a4
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-15 14:29:51 -0700

    add a test for an enemy constructor that has a meaningful fail

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -1,6 +1,7 @@
 import { Character } from "./../src/rpg.js";
 import { Item } from "./../src/rpg.js";
 import { Power } from "./../src/rpg.js";
+import { Enemy } from "./../src/rpg.js";
 
 describe ('Character', () => {
   test('should create a character with a name, empty inventory, character class, experience points of 0, level 1, and first power', () => {
@@ -14,26 +15,61 @@ describe ('Character', () => {
 describe ("Pick Up Item", () => {
   test("should add a new item to the inventory if a player is holding less than five items", () => {
     let jessica = new Character ("Jessica", "Wizard", "Magic Missle");
-    let sword = new Item ("Sword", "Damage");
+    let sword = new Item ("Sword", "Damage", 5);
     jessica.pickUpItem(sword);
     expect(jessica.inventory).toEqual([sword]);
   });
 });
 
+// describe ("Attack Enemy - Weapon", () => {
+//   test("should reduce health of enemy with the character's strength plus the weapon's damage", () => {
+//     let jessica = new Character ("Jessica", "Wizard", "Magic Missle");
+//     let sword = new Item ("Sword", "Damage", 5);
+//   })
+// })
+
+
+// Character.prototype.attackEnemyWeapon = function(enemy, item) { 
+//   if (item.effect === "damage") {
+//     enemy.health -= (item.strength + this.strength);
+//   }
+// };
+
+// Character.prototype.attackEnemyPower = function(enemy, power) { 
+//   if(enemy.weakness === power.type) {
+//     enemy.health -= ((power.strength * 2) + this.strength);
+//   } else {
+//     enemy.health -= (power.strength + this.strength);
+//   } 
+// };
+
 describe('Item', () => {
   test('should create an item object with name and effect', () => {
-    let item = new Item("Sword", "Damage");
+    let item = new Item("Sword", "Damage", 5);
     expect(item.name).toEqual("Sword");
     expect(item.effect).toEqual("Damage");
+    expect(item.strength).toEqual(5);
   }); 
 });
   
 describe('Power', () => {
   test('should create a power object with name and damage', () => {
-    let power = new Power("Magic Missle", 5);
+    let power = new Power("Magic Missle", 5, "fire");
     expect(power.name).toEqual("Magic Missle");
     expect(power.damage).toEqual(5);
+    expect(power.type).toEqual("fire");
   });
 });
+
+describe('Enemy', () => {
+  test("should create an ememy with a name, health, and weakness", () => {
+    let goblin = new Enemy("goblin", 11, "fire");
+    expect(goblin.name).toEqual("goblin");
+    expect(goblin.health).toEqual(11);
+    expect(goblin.weakness).toEqual("fire");
+  });
+});
+
+
 
 
--- src/rpg.js
+++ src/rpg.js
@@ -5,6 +5,7 @@ export function Character(userName, userClass, userPower) {
   this.power = [userPower];
   this.experience = 0;
   this.level = 1;
+  this.strength = 5;
 }
 
 Character.prototype.pickUpItem = function(item) {
@@ -15,17 +16,46 @@ Character.prototype.pickUpItem = function(item) {
   }
 };
 
-export function Item(itemName, itemEffect) {
+
+// Character.prototype.attackEnemyWeapon = function(enemy, item) { 
+//   if (item.effect === "damage") {
+//     enemy.health -= (item.strength + this.strength);
+//   }
+// };
+
+// Character.prototype.attackEnemyPower = function(enemy, power) { 
+//   if(enemy.weakness === power.type) {
+//     enemy.health -= ((power.strength * 2) + this.strength);
+//   } else {
+//     enemy.health -= (power.strength + this.strength);
+//   } 
+// };
+
+
+export function Item(itemName, itemEffect, itemStrength) {
   this.name = itemName;
   this.effect = itemEffect;
+  this.strength = itemStrength;
 }
 
-export function Power(namePower, damagePower) {
+export function Power(namePower, damagePower, damageType) {
   this.name = namePower;
   this.damage = damagePower;
+  this.type = damageType;
+}
+
+export function Enemy(enemyName, enemyHealth, enemyWeakness) {
+  // this.name = enemyName;
+  // this.health = enemyHealth;
+  // this.weakness = enemyWeakness;
 }
 
 
+//Add type advantage for different enemies
+// sword = new Item("sword", "damage", 5);
+// wand = new Item("wand", "damage", 9);
+// goblin = new Enemy("goblin", "fire")
+// magicMissle = new Power("magic missle", 3, "fire")
 
 
   
