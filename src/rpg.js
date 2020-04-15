ef5521f04aec7e7dccb6e8066b8eb90d8e2aa3aa
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-15 10:43:03 -0700

    add character constructor that passes test

--- src/rpg.js
+++ src/rpg.js
@@ -1,8 +1,8 @@
 export function Character(userName, userClass, userPower) {
-  // this.name = userName;
-  // this.class = userClass;
-  // this.inventory = [];
-  // this.power = [userPower];
-  // this.experience = 0;
-  // this.level = 1;
+  this.name = userName;
+  this.class = userClass;
+  this.inventory = [];
+  this.power = [userPower];
+  this.experience = 0;
+  this.level = 1;
 }
